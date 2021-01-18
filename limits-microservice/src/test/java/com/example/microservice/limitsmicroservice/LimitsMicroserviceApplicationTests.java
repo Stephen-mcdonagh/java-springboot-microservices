e8e911e314daf3a4d37f2e7e66f11dffc3c62bca
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2021-01-18 17:40:01 +0000

    limits microservice init

---
+++ limits-microservice/src/test/java/com/example/microservice/limitsmicroservice/LimitsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.limitsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
