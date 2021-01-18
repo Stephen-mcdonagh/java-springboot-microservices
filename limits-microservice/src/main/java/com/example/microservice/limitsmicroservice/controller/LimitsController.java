cce54e26028b88f11e3ad606395e4d166744144a
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2021-01-18 18:06:11 +0000

    centralzed spring cloud server

--- limits-microservice/src/main/java/com/example/microservice/limitsmicroservice/controller/LimitsController.java
+++ limits-microservice/src/main/java/com/example/microservice/limitsmicroservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.example.microservice.limitsmicroservice.controller;
 
+import com.example.microservice.limitsmicroservice.Config.LimitsConfiguration;
 import com.example.microservice.limitsmicroservice.Model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private LimitsConfiguration limitsConfiguration;
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits(){
-		return new Limits(1,1000);
+		return new Limits(limitsConfiguration.getMinimum(),limitsConfiguration.getMaximum());
 	}
 }
