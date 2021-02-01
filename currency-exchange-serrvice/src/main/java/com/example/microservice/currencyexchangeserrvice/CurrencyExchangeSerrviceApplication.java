777e1c9dc4150ef84af1aa489fe221feff763cc5
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2021-02-01 14:24:29 +0000

    basic currency exchange MS added

---
+++ currency-exchange-serrvice/src/main/java/com/example/microservice/currencyexchangeserrvice/CurrencyExchangeSerrviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.currencyexchangeserrvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSerrviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeSerrviceApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-serrvice/src/test/java/com/example/microservice/currencyexchangeserrvice/CurrencyExchangeSerrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.currencyexchangeserrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSerrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
