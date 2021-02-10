ce8c7d7c5ad5d0b1c67df636130f232c380ed3f3
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2021-02-10 11:15:01 +0000

    connecting MS to zipkin for distributed tracing

--- currency-exchange-serrvice/src/main/java/com/example/microservice/currencyexchangeserrvice/Controller/CurrencyExchangeController.java
+++ currency-exchange-serrvice/src/main/java/com/example/microservice/currencyexchangeserrvice/Controller/CurrencyExchangeController.java
@@ -2,16 +2,19 @@ package com.example.microservice.currencyexchangeserrvice.Controller;
 
 import com.example.microservice.currencyexchangeserrvice.Model.CurrencyExchange;
 import com.example.microservice.currencyexchangeserrvice.Repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	private Environment environment;
@@ -21,6 +24,7 @@ public class CurrencyExchangeController {
 
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+		logger.info("retrieveExchangeValue called");
 
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from,to);
 		if(currencyExchange == null){
